bb454e6a992178af827474fba99723eb3033e1b3
Yamely-aa <1501495@senati.pe>
2024-07-30 14:41:01 -0500

    Agregue los ejercicios.js

---
+++ Ejercico_1.js
@@ -0,0 +1,5 @@
+//Escribe un programa que declare una variable y la inicialice con un valor. Luego, imprime el valor de la variable en la consola.
+
+
+let miVariable = "Hola, mi nombre es Yamely";
+console.log(miVariable);
